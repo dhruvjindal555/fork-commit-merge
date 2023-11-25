98ad2888347f15c7845e8c20fe053f470148d9f0
Dhruv jindal <124202030+dhruvjindal555@users.noreply.github.com>
2023-11-25 17:58:51 +0530

    Resolve the issue

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// React/Vite - Easy
+import "./index.css";
+
+function App() {
+  return (
+    <div className="center">
+      <h1>React/Vite - Easy </h1>
+      <div class="card">
+        <img src="https://loremflickr.com/320/240"/>
+        <h2>Cart Title</h2>
+        <p>Cart Description</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
